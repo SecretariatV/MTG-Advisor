4ae75f733e07cb7fb11426636c54d44ea54d9826
SecretariatV <oliver.boucher.25@gmail.com>
2025-05-03 10:26:56 -0400

    added gen-ai

--- App/backend/app.py
+++ App/backend/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from google import genai
+from dotenv import load_dotenv
+import os
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=gemini_api_key)
 
 app = Flask(__name__)
 # Cors allows you to communicate with React on different ports
