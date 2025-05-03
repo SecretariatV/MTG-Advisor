4ca733ecaaf61cbd907526847ec13c0ce1621391
SecretariatV <oliver.boucher.25@gmail.com>
2025-05-03 11:25:19 -0400

    Added Boiler plate

---
+++ App/src/component/insider-directory/insider-directory.component.jsx
@@ -0,0 +1,9 @@
+import './insider-directory.styles.css'
+
+const InsiderDirectory = () => {
+    <div className="insider-parent">
+        <p>This is the Insider</p>
+    </div>
+}
+
+export default InsiderDirectory;
