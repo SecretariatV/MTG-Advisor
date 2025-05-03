67913f32168ebee73c24fd4ac970c2d16ad1ca8e
SecretariatV <oliver.boucher.25@gmail.com>
2025-05-03 16:35:36 -0400

    testing chat

--- App/backend/chat.py
+++ App/backend/chat.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 import os
 
@@ -11,7 +12,23 @@ CORS(chat)
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=gemini_api_key)
-model = genai.GenerativeModel('gemini-2.0-flash')
+
+@app.route('/gemini')
+def communication():
+    aiChat = client.chats.create(
+        model="gemini-2.0-flash",
+        config = types.GenerateContentConfig(
+            system_instruction="""You are a stock advisor. You are interested in taking information of US senator
+            trades, as well as stock market data based on the time of the senator trades, and generating a 
+            prediction as to the success of the stock that is selected. You will create suggestions for the 
+            user based on this data, taking their prompt and replying in the mindset of a stock advisor"""
+        )
+    )
+
+    response = aiChat.send_message_stream()
+    for chunk in response:
+        print(chunk.text, end="")
+
 
 if __name__ == '__main__':
     chat.run(debug=True)
