116c4a6ce9baf96f3ca73472d7bcd7d9c750a163
SecretariatV <oliver.boucher.25@gmail.com>
2025-05-04 05:00:28 -0400

    Added Loading component

---
+++ App/src/component/loading/loading.component.jsx
@@ -0,0 +1,13 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const LoadingScreen = () => {
+    return (
+        <DotLottieReact
+            src="https://lottie.host/6fe97f70-e380-46f8-aa8f-03617b84d0ab/sVBUvnKWYg.lottie"
+            loop
+            autoplay
+        />
+    )
+}
+
+export default LoadingScreen
